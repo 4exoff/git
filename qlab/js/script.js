e5ae51963368268ecd4cfc0ef007733e0baf5125
4exoff <mr.alexancho@gmail.com>
2017-02-09 02:17:13 +0300

    Revert "qlab"
    This reverts commit cf00df94dc9e4dbd7808dd88a34d9a51f0270171.

--- qlab/js/script.js
+++
@@ -1,8 +0,0 @@
-$('document').ready(function(){
-    $(window).scroll(function() {
-        var top = $(document).scrollTop();
-        if (top < $(".headerImage").height()) $(".headerContent").css({top: '0', position: 'relative'});
-        else $(".headerContent").css({top: '0px', position: 'fixed',width:'100%'});
-        
-    });
-});
